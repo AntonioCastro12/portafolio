9235ea54ad17e22d7ae89585ab492e05a3e07ca0
CaroValadez <carolinavaladezv@gmail.com>
2026-03-05 15:45:23 -0600

    cambio de ruta

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,11 +3,11 @@ export const Hero = () => {
     <section className="relative min-h-screen flex items-center overflow-hidden">
       {/* Bg */}
       <div className="absolute inset-0">
-        <img
-          src="/public/hero-bg.jpgss"
-          alt="Hero image"
-          className="w-full h-full object-cover opacity-40"
-        />
+       <img
+  src="/hero-bg.jpg"
+  alt="Hero image"
+  className="w-full h-full object-cover opacity-40"
+/>
 
         <div className="absolute inset-0 bg-gradiente-to-b from-background/20 via-background/80 to-background"></div>
       </div>
