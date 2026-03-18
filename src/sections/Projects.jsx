0731eb0e9228ccb8a0460a1b8e9ef46da1e10f55
CaroValadez <carolinavaladezv@gmail.com>
2026-03-18 14:34:34 -0600

    Proyectos

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,13 +1,14 @@
 const projects = [
-    {
-        title: "Panel de tecnología financiera",
-        description: "Una plataforma integral de análisis financiero con visualización de datos en tiempo real, gestión de cartera e información basada en inteligencia artificial.",
-        image: "/projects/project4.png",
-        tags: ["React", "TypeScript", "NodeJS"],
-        link:"#",
-        github:"#"        
-    },
-     {
+  {
+    title: "Panel de tecnología financiera",
+    description:
+      "Una plataforma integral de análisis financiero con visualización de datos en tiempo real, gestión de cartera e información basada en inteligencia artificial.",
+    image: "/projects/project4.png",
+    tags: ["React", "TypeScript", "NodeJS"],
+    link: "#",
+    github: "#",
+  },
+  {
     title: "E-Commerce Platform",
     description:
       "A full-featured e-commerce solution with inventory management, payment processing, and analytics dashboard.",
@@ -34,13 +35,26 @@ const projects = [
     link: "#",
     github: "#",
   },
-]
-
+];
 
 export const Projects = () => {
-    return( 
+  return (
     <section id="projects" className="py-32 relativw overflow-hiden">
-        <div className="container mx-auto px-6 relative z-10"></div>
+      <div className="absolute top-1/4 right-0 w-96 h-96 bg-primary/5 rounded-full blur-3xl" />
+      <div className="absolute bottom-1/4 left-0 w-64 h-64 bg-highlight/5 rounded-full blur-3xl" />
+      <div className="container mx-auto px-6 relative z-10">
+
+        {/* Section Header*/}
+        <div className="text-center mx-auto max-w-3xl mb-16">
+          <span className="text-secondary-foreground text-sm font-medium tracking-winder uppercase animate-fade-in">
+            Trabajo Destacado
+          </span>
+          <h2 className="text-4xl md:text-5xl font-bold mt-4 mb-6 animate-fade-in animation-delay-100">
+            proyectos que
+            <span> Impactaron.</span>
+          </h2>
+        </div>
+      </div>
     </section>
-)
+  );
 };
